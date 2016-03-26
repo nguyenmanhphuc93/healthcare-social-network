d1e9acac1eed98ed28b79ec89d4cf4735dacdda0
duytran <trancongduy0612@gmail.com>
2016-03-26 20:55:31 +0700

    ...

---
+++ src/HeathcareSystem/Controllers/RoleController.cs
@@ -0,0 +1,28 @@
+﻿using Healthcare.Models;
+using Microsoft.AspNet.Authorization;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HeathcareSystem.Controllers
+{
+    [Authorize]
+    public class RoleController : BaseController
+    {
+        private readonly UserManager<User> _userManager;
+
+        public RoleController(IHealthcareContext context) : base(context)
+        {
+
+        }
+
+        public async Task<IActionResult> GetCurrentUserRoles()
+        {
+            var user = context.Users.SingleOrDefault(x => x.UserName == User.Identity.Name);
+            return Ok(await _userManager.GetRolesAsync(user));
+        }
+    }
+}
