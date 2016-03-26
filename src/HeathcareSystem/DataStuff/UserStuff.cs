91fc02da7de9515a17a1723701de5bff5f40d32f
Nguyen Phuc <nguyenmanhphuc@outlook.com>
2016-03-26 17:18:37 +0700

    Fix functions add stuff data

--- src/HeathcareSystem/DataStuff/UserStuff.cs
+++ src/HeathcareSystem/DataStuff/UserStuff.cs
@@ -9,7 +9,7 @@ namespace HeathcareSystem.DataStuff
 {
     public static class UserStuff
     {
-        public static void CreateUser(this UserManager<User> _usermanager, Profile profile,  string userName,string password, String role)
+        public static void CreateUser(this UserManager<User> _usermanager, Profile profile, string userName, string password, String role)
         {
             var user = new User()
             {
@@ -17,35 +17,41 @@ namespace HeathcareSystem.DataStuff
                 UserName = userName,
                 Profile = profile,
             };
-            _usermanager.CreateAsync(user, password);
-            _usermanager.AddToRoleAsync(user, role);
-          
-            
+            _usermanager.CreateAsync(user, password).Wait();
+            _usermanager.AddToRoleAsync(user, role).Wait();
+
+
         }
-        public static void SeedProfile(this HealthCareContext context, int count = 50)
+        public static List<Profile> SeedProfile(this HealthCareContext context, int count = 20)
         {
-            var mobiphones = SeedPhone(count);
-            var homePhones = SeedPhone(count);
-            for(var i = 0; i<count; i++)
+            //var mobiphones = SeedPhone(count);
+            //var homePhones = SeedPhone(count);
+            var profiles = new List<Profile>();
+            for (var i = 0; i < count; i++)
             {
-                context.Profiles.Add(new Profile()
+                profiles.Add(new Profile
                 {
                     FirstName = $"Văn {(char)('A' + i)}",
                     LastName = $"Nguyễn",
                     DisplayName = $"Văn {(char)('A' + i)}",
-                    MobiPhone = mobiphones[i],
-                    HomePhone = homePhones[i]
-                    
+                    //MobiPhone = mobiphones[i],
+                    //HomePhone = homePhones[i]
+
                 });
             }
+            context.AddRange(profiles);
+
+
             context.SaveChange();
+            return profiles;
         }
+
         public static List<string> SeedPhone(int count = 50)
         {
             var phones = new List<string>();
             var random = new Random();
-            
-            for(var i = 0; i< count; i++)
+
+            for (var i = 0; i < count; i++)
             {
                 var phone = "0";
                 for (var j = 0; j < 10; i++)
