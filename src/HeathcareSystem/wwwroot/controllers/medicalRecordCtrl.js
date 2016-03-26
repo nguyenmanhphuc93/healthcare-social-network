a56f91e627450ae4bfc506e699a7fbb7eadeb7a3
khanhnguyenhong <khanh.nguyen.k1set@eiu.edu.vn>
2016-03-26 21:57:00 +0700

    test support

--- src/HeathcareSystem/wwwroot/controllers/medicalRecordCtrl.js
+++ src/HeathcareSystem/wwwroot/controllers/medicalRecordCtrl.js
@@ -1,8 +1,19 @@
 ﻿app.controller('MedicalRecordCtrl', function ($scope, $http) {
     var getRecordStr = 'api/MedicalRecord/GetRecords';
     var record = {};
-    $scope.call = function () {
-        $http.get($scope.input)
+    $scope.post = function () {
+        $http.post('api/MedicalRecord/' + $scope.input, $scope.data)
+            .success(function (data) {
+                record = data;
+                console.log($scope.input);
+                console.log(data);
+            })
+            .error(function (error) {
+                console.log('error of ' + $scope.input);
+            })
+    }
+    $scope.get = function () {
+        $http.get('api/MedicalRecord/' + $scope.input)
             .success(function (data) {
                 record = data;
                 console.log($scope.input);
