c6972e4c8879f65facda3ce3a572f4ff0d820796
Nguyen Phuc <nguyenmanhphuc@outlook.com>
2016-03-26 13:46:21 +0700

    Remove dnx451 to optimize time for restoring

--- src/HeathcareSystem/Controllers/Appointment.cs
+++ src/HeathcareSystem/Controllers/Appointment.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNet.Mvc;
+using HeathcareSystem.BindingModels;
+using HeathcareSystem.Models;
 
 // For more information on enabling Web API for empty projects, visit http://go.microsoft.com/fwlink/?LinkID=397860
 
@@ -16,6 +18,14 @@ namespace HeathcareSystem.Controllers
         public IEnumerable<string> Get()
         {
             return new string[] { "value1", "value2" };
+
+        }
+
+        [HttpPost]
+        public IActionResult RequestAppointment(AppointmentRequestBindingModel model)
+        {
+            var appointmentRequest = new AppointmentRequest();
+            return Ok();
         }
     }
 }
