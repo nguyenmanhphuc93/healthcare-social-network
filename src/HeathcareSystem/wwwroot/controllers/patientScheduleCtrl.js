3f63559513f67f94c5cea47b0197a9edb0fb4cbb
Nguyen Phuc <nguyenmanhphuc@outlook.com>
2016-03-26 23:59:56 +0700

    Commit schedule

--- src/HeathcareSystem/wwwroot/controllers/patientScheduleCtrl.js
+++ src/HeathcareSystem/wwwroot/controllers/patientScheduleCtrl.js
@@ -1,5 +1,39 @@
-﻿app.controller("PatientScheduleCtrl", function ($scope) {
+﻿app.controller("PatientScheduleCtrl", function ($scope, $http) {
     $scope.$on('$viewContentLoaded', function () {
         $('#recheck').datetimepicker();
     });
+
+
+    var getIncommingPatient = function () {
+        return $http({
+            method: 'GET',
+            url: '/api/Appointment/GetDoctorInCommingAppoitnment',
+        });
+    }
+
+    var startExamination = function (exam) {
+        return $http({
+            method: 'GET',
+            url: '/api/MedicalRecord/CreateRecord',
+            data: exam
+        });
+    }
+
+    getIncommingPatient().success(function (data) {
+        $scope.patients = data;
+        for (var i = 0; i < data.length; ++i) {
+            $scope.patients[i].time = moment($scope.patients[i].time);
+        }
+    })
+
+    $scope.currentDate = moment().format("DD/MM/YYYY");
+    //console.log($scope.currentDate)
+
+    $scope.startExam = function (appointment) {
+        $scope.curPatient = appointment;
+        $scope.form = { appointmentId: appointment.id };
+        $scope.showPatientForm = true;
+
+    }
+
 });
