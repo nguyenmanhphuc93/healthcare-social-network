ffa4d091444427980ddf9188510b445651147ff3
ratulf <ratulf.passion@gmaiil.com>
2016-03-26 15:07:12 +0700

    ,,,,

--- src/HeathcareSystem/Models/Disease.cs
+++ src/HeathcareSystem/Models/Disease.cs
@@ -7,7 +7,7 @@ namespace HeathcareSystem.Models
 {
     public class Disease
     {
-        public int Id { get; set; }
+        public long Id { get; set; }
         public string Name { get; set; }
         public string Description { get; set; }
     }
